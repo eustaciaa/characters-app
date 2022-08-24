56a03045c1394f9edfb17534f03943feeca5a2cd
eustaciaa <clara3@student.umn.ac.id>
2022-08-24 19:17:08 +0700

    navbar and its routing created

--- characters-app/src/App.js
+++ characters-app/src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import React from "react";
 import './App.css';
 import Navbar from './Navbar';
+import All from './pages/All';
+import Locations from './pages/Locations';
+import Episodes from './pages/Episodes';
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   // return (
@@ -25,7 +29,17 @@ function App() {
   // );
 
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <div class="container">
+        <Routes>
+          <Route path="/" element={<All />}/>
+          <Route path="/all" element={<All />}/>
+          <Route path="/by-locations" element={<Locations />}/>
+          <Route path="/by-episodes" element={<Episodes />}/>
+        </Routes>
+      </div>
+    </div>    
   )
 }
 export default App;
--- characters-app/src/Navbar.js
+++ characters-app/src/Navbar.js
@@ -1,25 +1,21 @@
+import { NavLink } from "react-router-dom"
+
 export default function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Characters</a>
+        <NavLink to="/" className="navbar-brand">
+          Characters
+        </NavLink>
+        {/* <a class="navbar-brand" href="#">Characters</a> */}
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">All</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">By Location</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">By Episode</a>
-            </li>
-            {/* <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li> */}
+            <CustomLink to="/all">All</CustomLink>
+            <CustomLink to="/by-locations">By Location</CustomLink>
+            <CustomLink to="/by-episodes">By Episode</CustomLink>
           </ul>
           {/* <form class="d-flex">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
@@ -28,5 +24,23 @@ export default function Navbar() {
         </div>
       </div>
     </nav>
+  )
+}
+
+function CustomLink({ to, children, ...props }){
+  const path = window.location.path
+
+  return (
+    <li className="nav-item">
+      <NavLink 
+        to={to}  
+        className={({ isActive }) =>
+          isActive ? "nav-link active" : "nav-link"
+        }
+        {...props}
+      >
+        {children}
+      </NavLink>
+    </li>
   )
 }
